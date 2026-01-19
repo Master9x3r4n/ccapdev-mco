7dbcab134b40a9229041563867204d873c6c3749
Stephen <stephen.jszs.gplay@gmail.com>
2026-01-19 23:55:08 +0800

    finally got things working i hate this

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LandingView from '../view/LandingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
