92ac2229d07057d9bce86ac87f4d950c293299cd
Stephen <stephen.jszs.gplay@gmail.com>
2026-01-30 22:58:49 +0800

    feat: added implementation for dark mode

--- src/view/LandingView.vue
+++ src/view/LandingView.vue
@@ -1,7 +1,22 @@
 <template>
-  <main>Hello World</main>
-  Hello
+    <p class ="bg-white dark:bg-black dark:text-white"> Hello </p>
+  	<button class="border p-2 m-4 active:bg-gray-400" @click="toggleDarkMode()">Click Me!</button>
 </template>
 
-<script setup></script>
+<script setup>
+	import {ref, watch} from "vue"
+
+	const darkMode = ref(false)
+
+	watch(darkMode, (enabled) => {
+    	document.documentElement.classList.toggle('dark', enabled)
+  	})
+
+	const toggleDarkMode = () => {
+		darkMode.value = !darkMode.value
+		console.log(darkMode.value)
+	}
+
+</script>
+
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 export default {
+  darkMode: 'class',
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
