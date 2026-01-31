0433c571335c0286b84c988a2cc2b3d0530de2c8
Stephen <stephen.jszs.gplay@gmail.com>
2026-01-31 21:13:12 +0800

    adjusted dark mode button in playground view

--- src/view/PlaygroundView.vue
+++ src/view/PlaygroundView.vue
@@ -5,5 +5,17 @@ import DarkModeButton from '@/components/DarkModeButton.vue';
 </script>
 
 <template>
-	<DarkModeButton />
+    <!-- Dark Mode Button -->
+	<div class ="p-4 flex justify-center items-center">
+        <span class = "m-1 p-2 border-4 rounded-2xl text-white
+        bg-[#169cda] border-[#0f77a7]
+        dark:text-white dark:bg-[#0c2f8e] dark:border-[#0a2878]">
+            Theme 
+        </span>
+        <DarkModeButton />
+    </div>
+
+    <!-- Carousel -->
+
+
 </template>
