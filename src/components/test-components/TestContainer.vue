9f25c7a974c80a228ddb65fe1061ff8cef7da074
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-03 15:30:28 +0800

    added test components

---
+++ src/components/test-components/TestContainer.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const props = defineProps(['dir'])
+</script>
+
+<template>
+    <div class="p-4 flex justify-center items-center border-b-4 bg-white 
+        dark:bg-[#656565] dark:border-[#494949]"
+        :class="(props.dir == 'row')? 'flex-row' : 'flex-col'">
+        <slot></slot>
+    </div>
+</template>
---
+++ src/components/test-components/TestLabel.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <span class="m-1 p-2 border-4 rounded-2xl text-white
+        bg-[#169cda] border-[#0f77a7]
+        dark:text-white dark:bg-[#0c2f8e] dark:border-[#0a2878] dark:font-bold">
+        <slot></slot>
+    </span>
+</template>
