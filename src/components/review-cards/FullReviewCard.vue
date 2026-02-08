e10c6b8c6df248e6e5e276ca98034937580dedcb
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-08 22:55:39 +0800

    feat: revamped media container and full review card

--- src/components/review-cards/FullReviewCard.vue
+++ src/components/review-cards/FullReviewCard.vue
@@ -1,13 +1,16 @@
 <script setup>
+import Carousel from '../carousel/Carousel.vue';
+import MediaContainer from '../carousel/MediaContainer.vue';
+import ThumbsButton from '../thumbs-buttons/ThumbsButton.vue';
 
 </script>
 
 <template>
     <!-- 1120 x 769 -->
-    <div class="w-280 h-192 p-10 flex flex-col items-start gap-6 border-[#BFBFBF] border-3 rounded-3xl 
+    <div class="w-295 h-228 p-5 flex flex-col items-center justify-center gap-6 border-b-[#BFBFBF] border border-b-3
     bg-white">
         <!-- Header Container -->
-        <div class="w-full flex justify-between items-center border">
+        <div class="w-full flex justify-between items-center">
             <div class="flex gap-3 items-center">
                 <!-- Profile -->
                 <div class="w-13 h-13 rounded-[50%] bg-amber-100 bg-gradient"></div>
@@ -27,14 +30,14 @@
         </div>
 
         <!-- Title Container -->
-        <div class="w-full h-[14%] flex items-center font-bold leading-8 text-4xl border">
+        <div class="w-full h-[8%] flex items-center font-bold leading-8 text-4xl pl-2 pr-2">
             <slot name = "review-title">Title of Review</slot>
         </div>
 
         <!-- Comment Container -->
-        <div class="h-[56%] flex flex-col border">
+        <div class="flex flex-col justify-center grow gap-3 w-286 m-0">
             <!-- Review Container -->
-            <div class="w-full text-[16px] leading-6 grow">
+            <div class="w-[99%] text-[20px] leading-6 grow pl-3 pr-3">
                 <slot name="review">
                     I have stayed at this apartment for a while, and let me say, it is as the name says. 
                     It's a really cool apartment and it has a lot of the amenities 
@@ -42,24 +45,42 @@
                 </slot>
             </div>
 
-            <!-- CHANGE THIS TO CONDITIONAL SLOTTING -->
-            <template v-if="$slots.ownerReply">
-                <!-- Reply Container -->
-                <div class="w-full h-[50%] mt-3">
-                    <div class="bg-[#D9D9D9] h-full rounded-3xl 
-                    flex flex-col justify-start items-center p-5.5 gap-1.25">
-                        <div class="italic text-left w-full text-[14px] leading-6">
-                            Reply from the owner
+            <!-- Carousel Container -->
+            <div class="mt-3 h-[47%] flex w-full justify-center items-center">
+                <Carousel :count="4" buttonStyling="large" buttonSpacing="4">
+                <template #content>
+                    <template v-for="i in 10">
+                        <div class="flex shrink-0 snap-start pl-2 pr-2">
+                            <MediaContainer size="medium"/>
                         </div>
-                        <div class="w-full text-[16px] leading-6">
-                            <slot name="ownerReply">
-                            </slot>
-                        </div>
+                    </template>
+                </template>
+                </Carousel>
+            </div>
+
+            <!-- Reply Container -->
+            <div v-if="$slots.ownerReply" class="w-full h-[23%] mt-3 pl-2 pr-2">
+                <div class="bg-[#D9D9D9] h-full rounded-3xl
+                flex flex-col justify-start items-center p-5.5 gap-1.25">
+                    <div class="italic text-left w-full text-[14px] leading-6">
+                        Reply from the owner
+                    </div>
+                    <div class="w-full text-[16px] leading-6">
+                        <slot name="ownerReply"></slot>
                     </div>
                 </div>
-            </template>
+            </div>
         </div>
 
+        <!-- Footer Container -->
+        <div class="w-full h-[7%] flex justify-start items-center pl-2 pr-2">
+            <!-- Upvote -->
+            <div class="italic font-normal text-[16px] leading-6 flex items-center justify-around gap-2">
+                <ThumbsButton direction="up"/>
+                <div>-69</div>
+                <ThumbsButton direction="down"/>
+            </div>
+        </div>
 
     </div>
 
