1f7ad8b98fc7470e363fee41b8ef12fb673e8429
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-09 22:01:26 +0800

    feat: added landing page

---
+++ src/components/landing/LandingCategory.vue
@@ -0,0 +1,55 @@
+<script setup>
+import ApartmentCardSmall from '../apartment-cards/ApartmentCardSmall.vue';
+import Carousel from '../carousel/Carousel.vue';
+import FilterButton from '../filter-button/FilterButton.vue';
+
+</script>
+
+<template>
+<div class="h-fit flex flex-col w-full gap-3">
+    <!-- Text -->
+    <div class="italic font-[20px] leading-6 font-light p-4">
+        <slot name="text">Find homes near your university</slot>
+    </div>
+
+    <!-- Filters -->
+    <div class="flex items-center justify-around overflow-x-scroll">
+        <template v-for="i in 5">
+            <FilterButton/>
+        </template>
+    </div>
+
+    <!-- Carousel Container-->
+    <div class="h-fit w-full flex justify-start">
+        <Carousel :count="2"  buttonStyling="small circular" :buttonSpacing="1">
+            <template #content>
+            <template v-for="i in 5">
+                <div class="px-3 flex shrink-0 snap-start">
+                <ApartmentCardSmall
+                :ratingData="{rating: 5 - i + 0.5, reviewCount: (i)* 6}">
+                    <template #header>
+                        Apartment {{ i }}
+                    </template>
+                </ApartmentCardSmall>
+                </div>
+            </template>
+            </template>
+        </Carousel>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+/* hide scrollbar for webkit browsers */
+.hide-scrollbar::-webkit-scrollbar {
+    display: none;
+}
+
+/* hide scrollbar for others */
+.hide-scrollbar {
+    -ms-overflow-style: none;  
+    scrollbar-width: none;  
+}
+
+</style>
--- src/view/LandingView.vue
+++ src/view/LandingView.vue
@@ -1,10 +1,57 @@
-<template>
-	<DarkModeButton />
-</template>
-
 <script setup>
+import SearchBar from '@/components/header/SearchBar.vue';
+import LandingCategory from '@/components/landing/LandingCategory.vue';
+
+
 
 
 </script>
+
+<template>
+<div class="flex flex-col justify-around gap-8 w-full h-fit">
+	<!-- Hero Section -->
+	<div class="w-full h-113 bg-blue-200 flex justify-end">
+		<div class="h-full w-[80%] bg-gradient flex flex-col items-end text-right text-white gap-2">
+			<div class="mr-12 mt-14 w-[64%]">
+				<!-- Main text -->
+				<div class="text-6xl leading-20">
+					We find homes <span class="underline italic font-bold">fantastic</span> for you!
+				</div>
+				<!-- Subtext -->
+				<div class=" text-2xl leading-7 mt-5">
+					Find the best fitting apartments and condominiums for you, rated by 
+					<span class="underline italic">students just like you!</span>
+				</div>
+			</div>
+
+			<!-- Search bar -->
+			<div class="w-full h-full relative">
+				<div class="absolute right-2 top-0 w-148">
+					<SearchBar type="alt"/>
+				</div>
+			</div>		
+		</div>
+	
+	</div>
+
+	<!-- Main Section -->
+	<div class="w-full h-fit flex flex-col gap-10 justify-start px-12 pb-4">
+		<!-- First Category -->
+		<LandingCategory/>
+
+		<!-- Second Category -->
+		<LandingCategory/>
+	</div>
+
+</div>
+</template>
+
+<style scoped>
+
+.bg-gradient {
+	background: linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, #355AFF 27.7%);
+}
+
+</style>
 
 
