4abef39687a50637c7a6afb7e95ac2a0622255e2
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-09 22:29:20 +0800

    fix: centered views

--- src/view/ReviewsView.vue
+++ src/view/ReviewsView.vue
@@ -9,19 +9,21 @@ const results = ref(3)
 </script>
 
 <template>
-<div class="flex flex-col justify-around gap-4 m-7 w-fit">
-    <!-- Body Header -->
-    <div class="w-full flex justify-between items-center px-4.5">
-        <!-- Search Result -->
-        <div class="font-light font-[20px] italic leading-6">Found {{ results }} reviews...</div>
-    </div>
+<div class="flex justify-center w-full">
+    <div class="flex flex-col justify-around gap-4 m-7 w-fit">
+        <!-- Body Header -->
+        <div class="w-full flex justify-between items-center px-4.5">
+            <!-- Search Result -->
+            <div class="font-light font-[20px] italic leading-6">Found {{ results }} reviews...</div>
+        </div>
 
-    <!-- Body Content -->
-    <div class="h-fit w-full flex flex-col gap-6">
-        <template v-for="i in results">
-        <div><FullReviewCard/></div>
-        <Divider v-if="i != results"/>
-        </template>
+        <!-- Body Content -->
+        <div class="h-fit w-full flex flex-col gap-6">
+            <template v-for="i in results">
+            <div><FullReviewCard/></div>
+            <Divider v-if="i != results"/>
+            </template>
+        </div>
     </div>
 </div>
 </template>
