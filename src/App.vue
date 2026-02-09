4c14f76c020032ce38f20d9c34faf9f247c6a7b7
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-09 10:35:17 +0800

    nit: moved some shit for consistency

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
+<script setup>
+  import {RouterView} from "vue-router";
+  import FooterComponent from "./components/footer/FooterComponent.vue";
+</script>
+
 <template>
   <div>
     <RouterView />
     <FooterComponent/>
   </div>
 </template>
-
-<script setup>
-  import {RouterView} from "vue-router";
-  import FooterComponent from "./components/footer/FooterComponent.vue";
-</script>
 
 <style lang = "scss" scoped></style>
