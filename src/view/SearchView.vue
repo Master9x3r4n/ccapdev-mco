4ba7823dd62861e8b99d42d6bc5a650cb9ef84b9
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-09 23:32:19 +0800

    feat: Added dummy data to search view

--- src/view/SearchView.vue
+++ src/view/SearchView.vue
@@ -1,6 +1,42 @@
 <script setup>
 import ApartmentCardLarge from '@/components/apartment-cards/ApartmentCardLarge.vue';
 
+const props = defineProps({
+    searchQuery: {
+        type: String,
+        default: "beautiful apartment"
+    },
+    searchResults: {
+        type: Array,
+        default: [
+        {
+            name: "Grand Apartment", 
+            description: "A very grand, beautiful, and luxurious apartment that features multiple rooms and doors. A must stay for the holidays.",
+            ratingData: {
+                rating: 4.5,
+                reviewCount: 124,
+            }
+        },
+        {
+            name: "The Baxter Suite",
+            description: "Inspired by the heroes of the Fantastic Four, this beautiful deluxe suite features 4 bedrooms and a visit from Galactus. It's Fantastic.",
+            ratingData: {
+                rating: 4,
+                reviewCount: 44,
+            }
+        },
+        {
+            name: "Miro's House",
+            description: "Freshly doxxed, Miro's house is a beautiful home that features Miro.",
+            ratingData: {
+                rating: 0.5,
+                reviewCount: 67,
+            }
+        }
+        ]
+    }
+})
+
 </script>
 
 <template>
@@ -9,7 +45,7 @@ import ApartmentCardLarge from '@/components/apartment-cards/ApartmentCardLarge.
         <!-- Body Header -->
         <div class="w-full flex justify-between items-center px-2">
             <!-- Search Result -->
-            <div class="font-light font-[20px] italic leading-6">Found 2 results for “beautiful apartment”</div>
+            <div class="font-light text-[20px] italic leading-6">Found {{ props.searchResults.length }} results for “{{ props.searchQuery }}”</div>
 
             <!-- Filter Button -->
             <div class="flex justify-center items-center py-1 px-3.75 border-2 rounded-[40px]
@@ -20,8 +56,17 @@ import ApartmentCardLarge from '@/components/apartment-cards/ApartmentCardLarge.
 
         <!-- Body Content -->
         <div class="h-fit w-full flex flex-col gap-8">
-            <template v-for="i in 2">
-            <div><ApartmentCardLarge/></div>
+            <template v-for="i in props.searchResults">
+            <div>
+            <ApartmentCardLarge :ratingData="i.ratingData">
+                <template #header>
+                    {{ i.name }}
+                </template>
+                <template #description>
+                    {{ i.description }}
+                </template>
+            </ApartmentCardLarge>
+            </div>
             </template>
         </div>
     </div>
