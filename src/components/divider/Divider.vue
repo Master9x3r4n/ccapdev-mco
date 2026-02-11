0a9f85e654d653d89adcb6d71fad847339a2d836
nathan <natetrndd@gmail.com>
2026-02-11 20:31:32 +0800

    feat: add dark mode transition

--- src/components/divider/Divider.vue
+++ src/components/divider/Divider.vue
@@ -1,3 +1,5 @@
+<script setup>
+</script>
 <template>
-<div class="border-b-2 border-[#C5C5C5] dark:border-[#3C4583] w-full my-2"></div>
+<div class="border-b-2 border-[#C5C5C5] dark:border-[#3C4583] w-full my-2 transition-colors duration-200"></div>
 </template>
